8525e7428b5ef3b65147e94e0f9acf2dd97ab45e
Firas Najjar <firas.najjar@sap.com>
2015-08-03 15:35:12 +0300

    cleanup

--- DragAndDrop.spec.js
+++ DragAndDrop.spec.js
@@ -1,15 +1,10 @@
 /**
  * Created by Firas Najjar on 14/07/2015.
  */
-/**
- * Created by I300494 on 09/07/2015.
- */
 'use strict';
 
-var webdriver = require('selenium-webdriver'),
-    test = require('selenium-webdriver/testing');
+var webdriver = require('selenium-webdriver');
 
-global.$ = require('jQuery');//Global variable
 
 var By = webdriver.By,
     until = webdriver.until;
@@ -20,52 +15,27 @@ describe('The drag and drop capability', function () {
     this.timeout(100000);
     var driver;
 
-    test.before(function () {
+    before(function () {
         var caps = webdriver.Capabilities.chrome();
         driver = new webdriver.Builder().withCapabilities(caps).build();
         return driver.get(url);
     });
 
-    test.after(function (done) {
-        return driver.quit().then(function() {
+    after(function (done) {
+        return driver.quit().then(function () {
             done();
         });
     });
 
-    //test.xit(
-    //    'drag A and drop it on B',
-    //    function (done) {
-    //        driver.wait(until.elementLocated(By.js('return $("header:contains(\'A\')")[0]')), 5000).then(function(elementA) {
-    //               return driver.wait(until.elementLocated(By.js('return $("header:contains(\'B\')")[0]')), 5000).then(function(elementB) {
-    //                   return elementA.getInnerHtml().then(function(innerHtmlA) {
-    //                       var actionSequence =  new webdriver.ActionSequence(driver);
-    //                       return actionSequence.dragAndDrop(elementA, elementB).perform().then(function() {
-    //                           done();
-    //                       });
-    //                   })
-    //
-    //               });
-    //        });
-    //    }
-    //);
-
-
-    test.it(
-        'drag A and drop it on B',
-        function (done) {
-            driver.wait(until.elementLocated(By.css('.column[id="column-b"]')), 5000).then(function(elementA) {
-                return driver.wait(until.elementLocated(By.css('.column[id="column-a"]')), 5000).then(function(elementB) {
-                    return elementA.getInnerHtml().then(function(innerHtmlA) {
-                        var actionSequence =  new webdriver.ActionSequence(driver);
-                        return actionSequence.dragAndDrop(elementA, elementB).perform().then(function() {
-                            driver.sleep(10*1000);
-                            done();
-                        });
-                    })
-
+    it('drag A and drop it on B',function (done) {
+        driver.wait(until.elementLocated(By.css('.column[id="column-b"]')), 5000).then(function (elementA) {
+            return driver.wait(until.elementLocated(By.css('.column[id="column-a"]')), 5000).then(function (elementB) {
+                var actionSequence = new webdriver.ActionSequence(driver);
+                return actionSequence.dragAndDrop(elementA, elementB).perform().then(function () {
+                    done();
                 });
             });
-        }
-    );
+        });
+    });
 
 });
